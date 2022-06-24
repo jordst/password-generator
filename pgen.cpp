c38e47c0e22b2980d9487077f933a732dcc3351d
jord <jrd@parocel.xyz>
2022-06-24 03:13:14 +0100

    indents, typo, array lengths

--- pgen.cpp
+++ pgen.cpp
@@ -22,25 +22,24 @@ std::string passGen(std::string arr, int len, int rngx, int rngy);
 void toc(std::string str);
 
 int main(int argc, char *argv[]) {
-    std::string alhpabet = {"abcdefghijklmnopqrstuvwxyz"};
-    std::string alhpabetCaps = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-    std::string num = {"1234567890"};
-    std::string special = {"!@#$%\"^&*`'()_-+=~[]{};:,./<>\?"};
+	std::string alphabet = {"abcdefghijklmnopqrstuvwxyz"};
+	std::string alphabetCaps = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	std::string num = {"1234567890"};
+	std::string special = {"!@#$%\"^&*`'()_-+=~[]{};:,./<>\?"};
 	std::string hexa = {"0123456789ABCDEF"};
 	std::string vowel = {"01aeiouyAEIOUY"};
 
-    std::string finalPass = "";
-    std::string nuPass;
+	std::string finalPass = "";
+	std::string nuPass;
 
-    option(argc, argv);
-    
+	option(argc, argv);
 	for (int i=0; i<10; i++) {
-	if (flag.alpha==1) finalPass += passGen(alhpabet, flag.pLength, 0, 25);
-	if (flag.alphaCaps==1) finalPass += passGen(alhpabetCaps, flag.pLength, 0, 25);
-	if (flag.specials==1) finalPass += passGen(special, flag.pLength, 0, 32);
-	if (flag.nums==1) finalPass += passGen(num, flag.pLength, 0, 9);
-	if (flag.hex==1) finalPass += passGen(hexa, flag.pLength, 0, 15);
-	if (flag.vowels==1) finalPass += passGen(vowel, flag.pLength, 0, 13);
+		if (flag.alpha==1) finalPass += passGen(alphabet, flag.pLength, 0, alphabet.length()-1);
+		if (flag.alphaCaps==1) finalPass += passGen(alphabetCaps, flag.pLength, 0, alphabetCaps.length()-1);
+		if (flag.specials==1) finalPass += passGen(special, flag.pLength, 0, special.length()-1);
+		if (flag.nums==1) finalPass += passGen(num, flag.pLength, 0, num.length()-1);
+		if (flag.hex==1) finalPass += passGen(hexa, flag.pLength, 0, hexa.length()-1);
+		if (flag.vowels==1) finalPass += passGen(vowel, flag.pLength, 0, vowel.length()-1);
 
 	nuPass += passGen(finalPass, flag.pLength, 1, finalPass.length()-1);
 	std::cout<<nuPass<<std::endl;
@@ -51,10 +50,10 @@ int main(int argc, char *argv[]) {
 
 // array, password lenght, min range of array, max range of array
 std::string passGen(std::string arr, int len, int rngx, int rngy) {
-    std::mt19937_64 randr;
+	std::mt19937_64 randr;
 	std::uniform_int_distribution<std::mt19937_64::result_type> rng(rngx, rngy);
-    randr.seed(std::random_device()());
-    std::string ret;
+	randr.seed(std::random_device()());
+	std::string ret;
 	for (int i = 0; i < len; i++) {
 		ret += arr[rng(randr)];
 	}
